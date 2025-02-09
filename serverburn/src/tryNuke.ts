ef7abe8df0df0b62b5450913b4145ee78fa1bcf4
Paul Dejean <paulcdejean@gmail.com>
2025-02-09 15:37:22 -0600

    Don't crash if you don't have the needed programs.

--- serverburn/src/tryNuke.ts
+++ serverburn/src/tryNuke.ts
@@ -1,11 +1,11 @@
 import { NS } from "@ns";
 
 export function tryNuke(ns : NS, server : string) : boolean {
-  ns.brutessh(server)
-  ns.ftpcrack(server)
-  ns.relaysmtp(server)
-  ns.httpworm(server)
-  ns.sqlinject(server)
-  ns.nuke(server)
+  if(ns.fileExists("BruteSSH.exe", "home")) ns.brutessh(server)
+  if(ns.fileExists("FTPCrack.exe", "home")) ns.ftpcrack(server)
+  if(ns.fileExists("relaySMTP.exe", "home")) ns.relaysmtp(server)
+  if(ns.fileExists("HTTPWorm.exe", "home")) ns.httpworm(server)
+  if(ns.fileExists("SQLInject.exe", "home")) ns.sqlinject(server)
+  if(ns.fileExists("NUKE.exe", "home")) ns.nuke(server)
   return ns.hasRootAccess(server)
 }
