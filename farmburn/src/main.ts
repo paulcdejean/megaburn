c33f807334b9ef1e1e9c3b59ea38d9b43d405eb8
Paul Dejean <paulcdejean@gmail.com>
2025-02-12 21:01:31 -0600

    WE LOOPIN

--- farmburn/src/main.ts
+++ farmburn/src/main.ts
@@ -5,30 +5,32 @@ import { formulasHack } from "./hack/formulasHack";
 import { farmScript } from "./constants";
 
 export async function main(ns: NS): Promise<void> {
+  while(true) {
   // Will create data/network.json if it doesn't already exist
-  const network = manageNetwork(ns)
+    const network = manageNetwork(ns)
 
-  // Copy HGW scripts to network
-  for (const server of network.keys()) {
-    ns.scp(farmScript, server, "home")
-  }
-
-  // Determine the target
-  if (!ns.fileExists("data/target.txt")) {
-    ns.write("data/target.txt", "n00dles", "w")
-  }
-  const target = ns.read("data/target.txt")
-
-  // Cleanup the farm scripts on exit
-  ns.atExit(() => {
+    // Copy HGW scripts to network
     for (const server of network.keys()) {
-      ns.scriptKill(farmScript, server)
+      ns.scp(farmScript, server, "home")
     }
-  })
 
-  if(ns.fileExists("Formulas.exe", "home")) {
-    await formulasHack(ns, target, network)
-  } else {
-    await basicHack(ns, target, network)
+    // Determine the target
+    if (!ns.fileExists("data/target.txt")) {
+      ns.write("data/target.txt", "n00dles", "w")
+    }
+    const target = ns.read("data/target.txt")
+
+    // Cleanup the farm scripts on exit
+    ns.atExit(() => {
+      for (const server of network.keys()) {
+        ns.scriptKill(farmScript, server)
+      }
+    })
+
+    if(ns.fileExists("Formulas.exe", "home")) {
+      await formulasHack(ns, target, network)
+    } else {
+      await basicHack(ns, target, network)
+    }
   }
 }
