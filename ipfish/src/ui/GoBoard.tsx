d3382d4f6038ac58d6cfbe1636795b0dc1597403
Paul Dejean <paulcdejean@gmail.com>
2025-02-21 00:38:29 -0600

    Progress.

--- ipfish/src/ui/GoBoard.tsx
+++ ipfish/src/ui/GoBoard.tsx
@@ -7,11 +7,11 @@ function GoBoard() {
       <div className={css.boardBackground} >
         <table className={css.board}>
           <tr>
-            <td className={css.intersection} />
-            <td className={css.intersection} />
-            <td className={css.intersection} />
-            <td className={css.intersection} />
-            <td className={css.intersection} />
+            <td className={`${css.intersection} ${css.topleft}`} />
+            <td className={`${css.intersection} ${css.topmiddle}`} />
+            <td className={`${css.intersection} ${css.topmiddle}`} />
+            <td className={`${css.intersection} ${css.topmiddle}`} />
+            <td className={`${css.intersection} ${css.topright}`} />
           </tr>
           <tr>
             <td className={css.intersection} />
