781f4dabe7a5c43857de4939c2b0c3148bf9c790
Paul Dejean <paulcdejean@gmail.com>
2025-02-24 23:00:38 -0600

    whitespace

--- ipfish/rust/lib.rs
+++ ipfish/rust/lib.rs
@@ -13,5 +13,4 @@ pub fn wasmsquare(n: js_sys::Number) -> js_sys::Number {
 #[wasm_bindgen]
 pub fn get_analysis(board_history: js_sys::Array) {
   let current_board : Vec<u8> = js_sys::Uint8Array::new(&board_history.into_iter().last().unwrap()).to_vec();
-  
 }
