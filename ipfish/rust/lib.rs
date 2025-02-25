6fb6dafd74a5752bd9f9d7a339693f4494c1ed0f
Paul Dejean <paulcdejean@gmail.com>
2025-02-24 22:59:04 -0600

    Hammer hammer hammer.

--- ipfish/rust/lib.rs
+++ ipfish/rust/lib.rs
@@ -9,3 +9,9 @@ use wasm_bindgen::prelude::*;
 pub fn wasmsquare(n: js_sys::Number) -> js_sys::Number {
   return n.clone() * n;
 }
+
+#[wasm_bindgen]
+pub fn get_analysis(board_history: js_sys::Array) {
+  let current_board : Vec<u8> = js_sys::Uint8Array::new(&board_history.into_iter().last().unwrap()).to_vec();
+  
+}
