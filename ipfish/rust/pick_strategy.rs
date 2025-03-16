ff94de2c420e47523e0e2aefb86e5b22343f8153
Paul Dejean <paulcdejean@gmail.com>
2025-03-16 14:15:38 -0500

    Switch to ab when winning?

--- ipfish/rust/pick_strategy.rs
+++ ipfish/rust/pick_strategy.rs
@@ -2,12 +2,13 @@ use crate::minimax_ab_strategy::minimax_ab_strategy;
 use crate::minimax_mc_strategy::minimax_mc_strategy;
 use crate::board::{Board, BoardHistory};
 use crate::montecarlo_score::montecarlo_score;
+use crate::pass_move::pass_move;
 
 /// Heuristically pick a strategy based on the in game situation and give the evaluation of that strategy.
 pub fn pick_strategy(board: &Board, board_history: &BoardHistory, opponent_passed: bool) -> Vec<f64> {
-  let winning_position: f64 = 0.3;
+  let winning_position: f64 = 0.1;
 
-  let position_evaluation: f64 = montecarlo_score(board, board_history, 100);
+  let position_evaluation: f64 = montecarlo_score(&pass_move(board), board_history, 100);
 
   // Switch to alphabeta if we're in a winning position.
   if position_evaluation > winning_position {
