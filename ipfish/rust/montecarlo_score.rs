3d887da673a216f72ad00bb8e7d8140f0febf6ac
Paul Dejean <paulcdejean@gmail.com>
2025-03-15 23:03:27 -0500

    Ok monte carlo is working.

--- ipfish/rust/montecarlo_score.rs
+++ ipfish/rust/montecarlo_score.rs
@@ -7,9 +7,8 @@ use crate::pass_move::pass_move;
 use crate::point_state::PointState;
 use rand::seq::SliceRandom;
 use rand::seq::IndexedRandom;
-use rand::RngCore;
 use rand::SeedableRng;
-use rand_chacha::ChaCha12Rng;
+use rand_pcg::Pcg64Mcg;
 
 #[repr(i32)]
 #[derive(Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd, Debug)]
@@ -17,6 +16,8 @@ pub enum Winner {
   WhiteWin = 0,
   BlackWin = 1,
 }
+
+type RNG = Pcg64Mcg;
 
 /// Returns the percentage (f64 between 0 and 1) of monte carlo simulations that resulted in a black win.
 /// Doesn't play moves that would be self sacrifice for the oppoent to play during simulation.
@@ -27,7 +28,7 @@ pub enum Winner {
 /// * `board` - The board state to evaluate.
 /// * `simulation_count` - The number of montecarlo simulations to run.
 pub fn montecarlo_score(board: &Board, simulation_count: i32) -> f64 {
-  let mut rng: ChaCha12Rng = ChaCha12Rng::seed_from_u64(js_sys::Math::random().to_bits());
+  let mut rng: RNG = RNG::seed_from_u64(js_sys::Math::random().to_bits());
 
   let mut black_wins: i32 = 0;
 
@@ -38,7 +39,7 @@ pub fn montecarlo_score(board: &Board, simulation_count: i32) -> f64 {
   return f64::from(black_wins) / f64::from(simulation_count);
 }
 
-fn play_random_move(board: &Board, rng: &mut ChaCha12Rng) -> Option<Board> {
+fn play_random_move(board: &Board, rng: &mut RNG) -> Option<Board> {
   let legal_for_me: Box<[bool]> = get_legal_moves(&board, None);
   let legal_for_opponent: Box<[bool]> = get_legal_moves(&pass_move(board), None);
   let mut possible_moves: Vec<usize> = Vec::new();
@@ -77,14 +78,14 @@ fn play_random_move(board: &Board, rng: &mut ChaCha12Rng) -> Option<Board> {
   }
 }
 
-fn montecarlo_simulation(mut board: Board, rng: &mut ChaCha12Rng) -> Winner {
-  loop {
+fn montecarlo_simulation(mut board: Board, rng: &mut RNG) -> Winner {
+  for _ in 0..board.board.len() {
     match play_random_move(&board, rng) {
       Some(s) => {board = s;},
       None => break,
     }
   }
-  let result = final_score(&board);
+  let result: f64 = final_score(&board);
 
   if result > 0.0 {
     return Winner::BlackWin
