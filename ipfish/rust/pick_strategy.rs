c80e0b87424c597fe9e91dab2df877bde6551a22
Paul Dejean <paulcdejean@gmail.com>
2025-03-16 14:09:39 -0500

    Switch to alphabeta faster.

--- ipfish/rust/pick_strategy.rs
+++ ipfish/rust/pick_strategy.rs
@@ -1,13 +1,16 @@
 use crate::minimax_ab_strategy::minimax_ab_strategy;
 use crate::minimax_mc_strategy::minimax_mc_strategy;
 use crate::board::{Board, BoardHistory};
+use crate::montecarlo_score::montecarlo_score;
 
 /// Heuristically pick a strategy based on the in game situation and give the evaluation of that strategy.
 pub fn pick_strategy(board: &Board, board_history: &BoardHistory, opponent_passed: bool) -> Vec<f64> {
-  let late_game: usize = 20;
+  let winning_position: f64 = 0.3;
 
-  // Switch to alphabeta in the late game.
-  if board_history.len() > late_game {
+  let position_evaluation: f64 = montecarlo_score(board, board_history, 100);
+
+  // Switch to alphabeta if we're in a winning position.
+  if position_evaluation > winning_position {
     return minimax_ab_strategy(board, board_history, opponent_passed);
   } else {
     return minimax_mc_strategy(board, board_history, opponent_passed);
