6d53c2536cf3925397ceea2807c5b2a91a917cd7
Paul Dejean <paulcdejean@gmail.com>
2025-03-21 23:57:09 -0500

    Remove auto tengen.

--- ipfish/rust/pick_strategy.rs
+++ ipfish/rust/pick_strategy.rs
@@ -9,17 +9,14 @@ use crate::RNG;
 pub fn pick_strategy(board: &Board, board_history: &BoardHistory, opponent_passed: bool, rng:&mut RNG) -> Vec<f64> {
   
   // Switch to alphabeta if we're in a winning position.
-  let winning_position: f64 = 0.4;
+  let winning_position: f64 = 0.35;
   let position_evaluation: f64 = montecarlo_score(board, board_history, 100, rng);
-  let mut result: Vec<f64>;
+  let result: Vec<f64>;
   if position_evaluation > winning_position {
     result = minimax_ab_strategy(board, board_history, opponent_passed);
   } else {
     result = minimax_mc_strategy(board, board_history, opponent_passed, rng);
   }
 
-  // For the first move, artifically boost the center point.
-  let center_point: usize = board.board.len() / 2;
-  result[center_point] = result[center_point] + 0.2;
   return result;
 }
