8a235c279d20fc0743c39475baf7e45f490691ab
Paul Dejean <paulcdejean@gmail.com>
2025-03-21 23:34:47 -0500

    20 win streak!

--- ipfish/rust/pick_strategy.rs
+++ ipfish/rust/pick_strategy.rs
@@ -2,7 +2,6 @@ use crate::minimax_ab_strategy::minimax_ab_strategy;
 use crate::minimax_mc_strategy::minimax_mc_strategy;
 use crate::board::{Board, BoardHistory};
 use crate::montecarlo_score::montecarlo_score;
-use crate::pass_move::pass_move;
 use crate::RNG;
 
 /// Heuristically pick a strategy based on the in game situation and give the evaluation of that strategy.
@@ -10,7 +9,7 @@ use crate::RNG;
 pub fn pick_strategy(board: &Board, board_history: &BoardHistory, opponent_passed: bool, rng:&mut RNG) -> Vec<f64> {
   
   // Switch to alphabeta if we're in a winning position.
-  let winning_position: f64 = 0.3;
+  let winning_position: f64 = 0.4;
   let position_evaluation: f64 = montecarlo_score(board, board_history, 100, rng);
   let mut result: Vec<f64>;
   if position_evaluation > winning_position {
