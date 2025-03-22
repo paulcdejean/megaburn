541e161ef0d8d3de0ce4d01c5fa824f84f84d2a8
Paul Dejean <paulcdejean@gmail.com>
2025-03-21 22:06:25 -0500

    Did I fix this stupid strategy?

--- ipfish/rust/helpless_minimax_strategy.rs
+++ ipfish/rust/helpless_minimax_strategy.rs
@@ -1,11 +1,15 @@
 use core::f64;
 
+use crate::bitset::BitSet;
 use crate::board::{Board, BoardHistory};
 use crate::final_score::final_score;
 use crate::get_legal_moves::get_legal_moves;
+use crate::is_in_atari::is_in_atari;
 use crate::make_move::make_move;
+use crate::get_adjacent_points::get_adjacent_points;
 use crate::player::Player;
 use crate::pass_move::pass_move;
+use crate::point_state::PointState;
 
 /// This tries to maximize score assuming that the opponent is passing every turn.
 /// It also knows to pass if there's no way to increase your score.
@@ -79,7 +83,7 @@ fn minimax_helpless(board: &Board, board_history: &BoardHistory, depth: usize, h
                                                   &deeper_history,
                                                   depth - 1,
                                                   helpless_player,
-                                                  speed_penalty + 0.01
+                                                  speed_penalty + 0.01 + atari_penalty(point, board)
                                                  );
         // Maximizing.
         best_score = best_score.max(minimax_score);
@@ -93,13 +97,37 @@ fn minimax_helpless(board: &Board, board_history: &BoardHistory, depth: usize, h
                                                   &deeper_history,
                                                   depth - 1,
                                                   helpless_player,
-                                                  speed_penalty + 0.01
+                                                  speed_penalty + 0.01 + atari_penalty(point, board)
                                                  );
         // Minimizing.
         best_score = best_score.min(minimax_score);
       }
       let pass_score: f64 = score(board, speed_penalty, helpless_player);
       return best_score.min(pass_score);
+    }
+  }
+}
+
+
+/// Applies a small penalty to stones that would be immediately captured.
+fn atari_penalty(point: usize, board: &Board) -> f64 {
+  let mut known_liberty: Option<usize> = None;
+
+  for adjacent_point in get_adjacent_points(point, board) {
+    if board.board[adjacent_point] == PointState::Empty as u8 {
+      known_liberty = Some(adjacent_point);
+      break;
+    }
+  }
+
+  match known_liberty {
+    None => 1000.0, // Don't poke out your own eye!
+    Some(s) => {
+      if is_in_atari(point, &make_move(point, board), s, &mut BitSet::new()) {
+        return 20.0;
+      } else {
+        return 0.0;
+      }
     }
   }
 }
