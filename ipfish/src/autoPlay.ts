40a62fc1133fe53027c6365b9fae53e917b8c3f3
Paul Dejean <paulcdejean@gmail.com>
2025-04-02 00:14:45 -0500

    Blunder detection lol

--- ipfish/src/autoPlay.ts
+++ ipfish/src/autoPlay.ts
@@ -15,6 +15,11 @@ export async function autoPlay(ns : NS, boardSize: 5 | 7 | 9 | 13, opponent : Go
       } else {
         const bestMoveColumn = Math.floor(analysis.bestMove % game.boardSize)
         const bestMoveRow = Math.floor(analysis.bestMove / game.boardSize)
+        if (analysis.analysis[analysis.bestMove] < 0.2) {
+          ns.tprint("Blunder detected, gameplay stopped!")
+          ns.exit()
+        }
+
         await game.makeMove(bestMoveRow, bestMoveColumn)
       }
     }
