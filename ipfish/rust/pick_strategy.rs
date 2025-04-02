1eb51ab0936f48a5bcf8999a355166f19a0e6610
Paul Dejean <paulcdejean@gmail.com>
2025-04-01 20:51:34 -0500

    Real score?

--- ipfish/rust/pick_strategy.rs
+++ ipfish/rust/pick_strategy.rs
@@ -24,7 +24,7 @@ pub fn pick_strategy(board: Board, board_history: BoardHistory, opponent_passed:
     for point in legal_moves {
         let branch: &Node = tree.get([point].as_slice()).expect("Tree branch not found!");
         let branch_score: f64 = branch.blackwins.get() / (branch.blackwins.get() + branch.whitewins.get()) - 0.5;
-        result[point] = branch.whitewins.get();
+        result[point] = branch_score;
     }
 
     return result;
