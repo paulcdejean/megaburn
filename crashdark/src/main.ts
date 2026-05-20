3fa8c02417bba11a43e935df54fd34304f8f4ebe
Paul Dejean <paulcdejean@gmail.com>
2026-05-20 01:45:18 -0500

    Not that strange but can't scp them.

--- crashdark/src/main.ts
+++ crashdark/src/main.ts
@@ -10,7 +10,7 @@ export async function main(ns: NS): Promise<void> {
       for (const filename of ns.ls(host)) {
         if (filename !== ns.getScriptName() && filename !== "remotes/phish.js") {
           const lastFour = filename.slice(-4);
-          if (lastFour === ".txt" || lastFour === ".lit" || lastFour === ".cct") {
+          if (lastFour === ".txt" || lastFour === ".lit") {
             const result = ns.scp(filename, "darkweb");
             if (result === false) {
               ns.tprint(`Failed to copy ${filename} from ${host} to darkweb`)
@@ -18,6 +18,8 @@ export async function main(ns: NS): Promise<void> {
           } else if (lastFour === "ache") {
             const cache = ns.dnet.openCache(filename, false);
             ns.tprint(cache.message);
+          } else if (lastFour === ".cct") {
+            // noop
           } else {
             ns.tprint(`Found strange file: ${filename}`)
           }
