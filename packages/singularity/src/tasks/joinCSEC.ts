fd6177d208ef12b267258935fde29d9bdf5c26ba
Paul Dejean <paulcdejean@gmail.com>
2026-05-23 20:48:13 -0500

    too much ram

--- packages/singularity/src/tasks/joinCSEC.ts
+++ packages/singularity/src/tasks/joinCSEC.ts
@@ -1,8 +1,6 @@
 import type { NS } from "@ns";
 
 export async function joinCSEC(ns: NS): Promise<void> {
-	if (ns.singularity.checkFactionInvitations().includes("CyberSec")) {
-		ns.singularity.joinFaction("CyberSec");
-		ns.singularity.stopAction();
-	}
+	ns.singularity.joinFaction("CyberSec");
+	ns.singularity.stopAction();
 }
