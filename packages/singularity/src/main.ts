88d7d2ef61c35bc44e3d05a58eb95d56ba1e7db5
Paul Dejean <paulcdejean@gmail.com>
2026-05-27 19:52:24 -0500

    cleanup

--- packages/singularity/src/main.ts
+++ packages/singularity/src/main.ts
@@ -68,12 +68,3 @@ async function realMain(ns: NS): Promise<void> {
 }
 
 export { main as notMain, realMain as main };
-
-// if (
-// 	!ns
-// 		.ps()
-// 		.map((x) => x.filename)
-// 		.includes("batcher.js")
-// ) {
-// 	ns.run("batcher.js", 1);
-// }
