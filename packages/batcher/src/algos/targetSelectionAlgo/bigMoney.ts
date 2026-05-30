ef3f91510207c55880c11e98d8e4052c0423840e
Paul Dejean <paulcdejean@gmail.com>
2026-05-30 02:01:14 -0500

    impatience

--- packages/batcher/src/algos/targetSelectionAlgo/bigMoney.ts
+++ packages/batcher/src/algos/targetSelectionAlgo/bigMoney.ts
@@ -4,18 +4,18 @@ import type { Network } from "../../types";
 // oxlint-disable-next-line no-unused-vars
 export function bigMoney(ns: NS, network: Network): string {
 	let bestServer = "phantasy";
-	let bestTime = Infinity;
+	let bestTime = 180000; // 3 minutes I'm impatient!
 	for (const [serverName, serverData] of network) {
 		// Selective criteria for a phantasy upgrade.
 		if (
 			// First criteria: Able to hack.
 			serverData.hasAdminRights &&
 			// Second criteria: Hacking level more than double required.
-			serverData.requiredHackingSkill * 2 < ns.getHackingLevel() &&
+			serverData.requiredHackingSkill < ns.getHackingLevel() &&
 			// Third criteria: Good server growth.
 			serverData.serverGrowth > 60 &&
 			// Fourth criteria: Max money more than 10x phantasy max.
-			serverData.moneyMax > ns.getServerMaxMoney("phantasy") * 10 &&
+			serverData.moneyMax > ns.getServerMaxMoney("phantasy") * 100 &&
 			// Fifth criteria: Better weaken time than current best.
 			ns.getWeakenTime(serverName) < bestTime
 		) {
